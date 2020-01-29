f1de69340e0816ccfad5a43a987241a9846cc729
Sonja E. Hennessy <35819406+SHenn39910@users.noreply.github.com>
2020-01-28 21:29:50 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import HomePage from "./pages/HomePage";
+// import MoviePage from "./pages"
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
